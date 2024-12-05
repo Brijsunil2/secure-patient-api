d9cbc2581facb2c6fd507f053326c35cf8274c65
Sunil Brijlall <brijsunil2@gmail.com>
2024-12-04 20:35:54 -0500

    Implement error handling middleware

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 const app = express();
 const PORT = process.env.PORT || 6000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
